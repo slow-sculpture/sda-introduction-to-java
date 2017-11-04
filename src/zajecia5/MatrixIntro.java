e90da30ed3e4379c40f31530aee36e273ca22dc3
ChristianR <christian.rogowski@yahoo.pl>
2017-11-04 15:57:43 +0100

    Dodanie kodu z zajec5

---
+++ src/zajecia5/MatrixIntro.java
@@ -0,0 +1,27 @@
+package zajecia5;
+
+
+import java.util.Scanner;
+
+public class MatrixIntro {
+    public static void main(String[] args) {
+
+        //utowrzenie macierzy 3x3
+        int[][] matrix = new int[3][3];
+        //wypelnienie wartosciami od uzytkownika
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < matrix[i].length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        //wyswietlenie zawartosci
+        for (int i = 0; i < matrix[i].length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/zajecia5/Palindrom.java
+++ src/zajecia5/Palindrom.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Palindrom {
 
     public static boolean isPalindrom(String text) {
+        ////usuniecie spacji -> zamiana " " na "" oraz zmieniamy na male litery
+        ////oryginalny napis nie ulega zmianie
+        text = text.replace(" ", "").toLowerCase();
         for (int i = 0; i < text.length() / 2; i++) {
             if (text.charAt(i) != text.charAt(text.length() - 1 - i)) {
                 return false;
@@ -19,7 +22,7 @@ public class Palindrom {
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj teskt o dlugosci do 30 znakow: ");
         String text = sc.nextLine();
-        System.out.println(isPalindrom(text));
+        System.out.println("Napis: " + text + " jest palindromem: " + isPalindrom(text));
 
 
     }
