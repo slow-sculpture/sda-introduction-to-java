e15ed9f28da1bf7cf3cef2a1f1dd9c13f9258254
ChristianR <christian.rogowski@yahoo.pl>
2017-11-04 15:58:22 +0100

    Dodanie kodu z zajec5

--- src/zajecia5/MatrixIntro.java
+++ src/zajecia5/MatrixIntro.java
@@ -10,14 +10,14 @@ public class MatrixIntro {
         int[][] matrix = new int[3][3];
         //wypelnienie wartosciami od uzytkownika
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < matrix[i].length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = scanner.nextInt();
             }
         }
 
         //wyswietlenie zawartosci
-        for (int i = 0; i < matrix[i].length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.println(matrix[i][j]);
             }
